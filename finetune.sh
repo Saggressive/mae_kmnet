de5941a01094c201a327ec891b563fd83ac1ccf0
root <root@ai-platform-bjzm-ge10-kml3.idczw.hb1.kwaidc.com>
2022-09-25 15:07:48 +0800

    force

--- finetune.sh
+++ finetune.sh
@@ -5,18 +5,18 @@ export NCCL_SOCKET_IFNAME=eth
 export NCCL_IB_HCA=mlx5
 
 node_rank=$1
-name=debug/ft2
+name=ft/clip
 all_dir=/nlp_group/wuxing/suzhenpeng/mae_resnet/clip_output_dir/${name}
 mkdir ${all_dir}
 
 
-nohup python -m torch.distributed.launch --nnodes=1 --master_addr=10.116.147.13 --node_rank=${node_rank}  --nproc_per_node=8   --master_port 23332  \
+nohup python -m torch.distributed.launch --nnodes=4 --master_addr=10.116.150.143 --node_rank=${node_rank}  --nproc_per_node=8   --master_port 23332  \
     --use_env main_finetune.py  \
-    --finetune clip_output_dir/clip_pixel0.5_mid6/checkpoint-60.pth \
+    --finetune clip_output_dir/clip/checkpoint-299.pth \
     --output_dir ${all_dir} \
     --log_dir ${all_dir} \
     --accum_iter 1 \
-    --batch_size 128 \
+    --batch_size 32 \
     --model vit_base_patch16 \
     --epochs 100 \
     --blr 5e-4 --layer_decay 0.65 \
